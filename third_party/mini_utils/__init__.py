1801870e24a389a459046d224862aa83b189536b
KorinRez <41090788+KorinRez@users.noreply.github.com>
2026-02-02 12:58:04 +0200

    Update __init__.py

--- third_party/mini_utils/__init__.py
+++ third_party/mini_utils/__init__.py
@@ -1,2 +1,11 @@
+"""
+Copyright (c) 2025 Ilya Savenko
+Licensed under the MIT License
+
+Original Repository: https://github.com/ilyasa332/radar-bird-segmentation
+See LICENSE file in this directory for full license text.
+"""
+
 from .File_handling import *
 from .RadarScan import RadarScan
+
